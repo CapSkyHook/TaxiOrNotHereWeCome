c6c927721427cb989a7a8e4455a6bda4e07148f2
CapSkyHook <gautam.srinivasan9@gmail.com>
2018-04-29 23:24:10 -0400

    Committed what I have for now

--- src/main/scala/TaxiProject.scala
+++ src/main/scala/TaxiProject.scala
@@ -1,4 +1,4 @@
-
+package runtime
 import org.apache.spark._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SQLContext
@@ -8,6 +8,7 @@ import org.apache.hadoop.fs.Path
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+// import src.main.scala.DistanceCalculator.{Location, DistanceCalculator}
 
 // import org.apache.spark.SparkContext
 // import org.apache.spark.SparkContext._ 
@@ -38,6 +39,9 @@ object TaxiProject {
   val CITIBIKE_DATA_PATH = "hdfs:///user/tra290/BDAD/Citibike_data/*"
   val OUTPUT_DIR_FILE_PATH = "hdfs:///user/tra290/BDAD/finalProject/to_clustering_tmp/"
   val CLUSTER_DIR_FILE_PATH = "hdfs:///user/tra290/BDAD/finalProject/Clusters_tmp"
+  val PRESERVED_START_END_PTS_DIR_FILE_PATH = "hdfs:///user/tra290/BDAD/finalProject/start_end_data/"
+  val INPUT_TRAIN_DIR_FILE_PATH = "hdfs:///user/tra290/BDAD/finalProject/station_data/NYC_Transit_Subway_Entrance_And_Exit_Data.csv"
+  val TRAIN_OUTPUT_DIR_FILE_PATH = "hdfs:///user/tra290/BDAD/finalProject/station_data/processed"
 
   def preprocessDataAndSaveToDataframe(sc: org.apache.spark.SparkContext): Unit = {
     val y_t_rdd = sc.textFile(YELLOW_TAXI_DATA_PATH)
@@ -94,12 +98,48 @@ object TaxiProject {
 
     val loc = y_t_start_locs.union(y_t_end_locs).union(g_t_start_locs).union(g_t_end_locs).union(uber_locs).union(citibike_start_locs).union(citibike_end_locs).cache()
 
-    val all_loc_in_NYC = loc.filter(x => (x(0) < MAX_LATITUDE && x(0) > MIN_LATITUDE) && (x(1) < MAX_LONGITUDE && x(1) > MIN_LONGITUDE)).cache()
+    val all_loc_in_NYC = loc.filter(x => (x(0) < MAX_LATITUDE && x(0) > MIN_LATITUDE) && (x(1) < MAX_LONGITUDE && x(1) > MIN_LONGITUDE))
 
     all_loc_in_NYC.saveAsTextFile(OUTPUT_DIR_FILE_PATH)
   }
 
-  def loadDataPerformAnalysis(sc: org.apache.spark.SparkContext): Unit = {
+  def preprocessDataMaintainStartStop(sc: org.apache.spark.SparkContext): Unit = {
+    val y_t_rdd = sc.textFile(YELLOW_TAXI_DATA_PATH)
+    val g_t_rdd = sc.textFile(GREEN_TAXI_DATA_PATH)
+
+    val y_t_header = y_t_rdd.first()
+    val g_t_header = g_t_rdd.first()
+
+    val y_t_rdd1 = y_t_rdd.filter { line =>
+      if (line.toLowerCase != y_t_header.toLowerCase) {
+        line.toLowerCase != y_t_header.toLowerCase
+      } else {
+        println(line)
+        line.toLowerCase != y_t_header.toLowerCase
+      }
+    }
+    val g_t_rdd1 = g_t_rdd.filter(line => line != g_t_header)
+
+    val y_t_columns = y_t_header.split(",").toSeq
+    val g_t_columns = g_t_header.split(",").toSeq
+
+
+    val y_t_rdd_split = y_t_rdd1.map(line => line.split(",")).filter(line => line.size == y_t_columns.size && line(7).forall(_.isDigit)).map(line => (line(0), line(1), line(2), line(3), line(4), ( if (line(5) != "") line(5).toDouble else 0.0), ( if (line(6) != "") line(6).toDouble else 0.0), line(7), line(8), ( if (line(9) != "") line(9).toDouble else 0.0), ( if (line(10) != "") line(10).toDouble else 0.0), line(11), line(12), line(13), line(14), line(15), line(16), line(17)))
+
+    val g_t_rdd_split = g_t_rdd1.map(line => line.split(",")).filter(line => line.size == g_t_columns.size).map(line => (line(0), line(1), line(2), line(3), line(4), line(5).toDouble, line(6).toDouble, line(7).toDouble, line(8).toDouble, line(9), line(10), line(11), line(12), line(13), line(14), line(15), line(16), line(17), line(18), line(19)))
+
+    val y_t_loc = y_t_rdd_split.map(l => Array(Array(l._7, l._6), Array(l._11, l._10)))
+    
+    val g_t_loc = g_t_rdd_split.map(l => Array(Array(l._7, l._6), Array(l._9, l._8)))
+
+    val loc = y_t_loc.union(g_t_loc)
+
+    val all_loc_in_NYC = loc.filter(line => ((line(0)(0) < MAX_LATITUDE && line(0)(0) > MIN_LATITUDE) && (line(0)(1) < MAX_LONGITUDE && line(0)(1) > MIN_LONGITUDE)) && ((line(1)(0) < MAX_LATITUDE && line(1)(0) > MIN_LATITUDE) && (line(1)(1) < MAX_LONGITUDE && line(1)(1) > MIN_LONGITUDE)))
+  
+
+    val all_loc_in_NYC_csved = all_loc_in_NYC.map(locs => locs.flatten.mkString(","))
+    
+    all_loc_in_NYC_csved.saveAsTextFile(PRESERVED_START_END_PTS_DIR_FILE_PATH)
 
   }
 
@@ -115,27 +155,72 @@ object TaxiProject {
       val splitLine = line.drop(1).dropRight(1).split(",")
       Vectors.dense(splitLine(0).toDouble, splitLine(1).toDouble)
     }.cache()
-    val clusters = KMeans.train(dense_loc_in_nyc, 25, 25)
+    val clusters = KMeans.train(dense_loc_in_nyc, 25, 10)
     val centroids = clusters.clusterCenters
     sc.parallelize(centroids).saveAsTextFile(CLUSTER_DIR_FILE_PATH)
+  }
+
+  def findCorrespondingTrainStation(sc: org.apache.spark.SparkContext): Unit = {
+    val startStopData = sc.textFile(PRESERVED_START_END_PTS_DIR_FILE_PATH)
+    val subwayStationsData = sc.textFile(TRAIN_OUTPUT_DIR_FILE_PATH)
+    val clustersData = sc.textFile(CLUSTER_DIR_FILE_PATH)
+
+    val clusters = clustersData.map(line => line.drop(1).dropRight(1).split(",")).map(value => Location(value(0).toDouble, value(1).toDouble))
+
+    var trainToCoord: scala.collection.mutable.Map[String, Array[Location]] = scala.collection.mutable.Map()
+    var coordToTrain: scala.collection.mutable.Map[Location, Array[String]] = scala.collection.mutable.Map()
+
+    subwayStationsData.foreach { line =>
+      val data = line.drop(1).dropRight(1).split(",")
+
+      val coords = data(0).split("=").map(_.toDouble)
+      val location = Location(coords(0), coords(1))
+      val trains = data(1).split("=")
+
+      for (train <- trains) {
+          if (trainToCoord.contains(train)) {
+              trainToCoord(train) = trainToCoord(train) ++ Array(location)
+          } else {
+            trainToCoord += (train -> Array(location))
+          }
+      }
+
+      if (coordToTrain.contains(location)) {
+          coordToTrain(location) = coordToTrain(location) ++ trains
+      } else {
+        coordToTrain += (location -> trains)
+      }
+
+    }
+
+    val distCalc = new DistanceCalculatorImpl() // .calculateDistanceInKilometer(Location(10, 20), Location(40, 20))
+
+    // startStopData.filter()
+
+
   }
 
   def main(args: Array[String]) {
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val sparkContext = spark.sparkContext
+    import spark.implicits._
 
     if (!(this.testDirExist(OUTPUT_DIR_FILE_PATH, spark) && (sparkContext.wholeTextFiles(OUTPUT_DIR_FILE_PATH).count > 0)))  {
         this.preprocessDataAndSaveToDataframe(sparkContext)
     } else {
-      println("Data Already Created!")
+      println("------------------------------------Data Already Created!------------------------------------")
     }
     if (!(this.testDirExist(CLUSTER_DIR_FILE_PATH, spark) && (sparkContext.wholeTextFiles(CLUSTER_DIR_FILE_PATH).count > 0)))  {
         this.performKMeansClustering(sparkContext)
     } else {
-      println("Cluster Already Performed!")
+      println("------------------------------------Cluster Already Performed!------------------------------------")
+    }
+    if (!(this.testDirExist(PRESERVED_START_END_PTS_DIR_FILE_PATH, spark) && (sparkContext.wholeTextFiles(PRESERVED_START_END_PTS_DIR_FILE_PATH).count > 0)))  {
+        this.preprocessDataMaintainStartStop(sparkContext)
+    } else {
+      println("------------------------------------Start Stop Data Already Created!------------------------------------")
     }
 
-    this.loadDataPerformAnalysis(sparkContext)
     spark.stop()
   }
 }
